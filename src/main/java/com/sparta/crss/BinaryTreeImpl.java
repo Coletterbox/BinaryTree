07fd57593c5f0a232a741bd1eb470b45c4e68056
Colette Shaw <cshaw@spartaglobal.com>
2020-03-15 22:53:14 +0000

    Clean up commented out code

--- src/main/java/com/sparta/crss/BinaryTreeImpl.java
+++ src/main/java/com/sparta/crss/BinaryTreeImpl.java
@@ -24,67 +24,10 @@ public class BinaryTreeImpl implements BinaryTree {
         return numElements;
     }
 
-//    //TODO: fix this - it does not deal with current node properly
-//    @Override
-//    public void addElement(int element) {
-//        numElements++;
-//        int rootNodeValue = rootNode.getValue();
-//        int leftChildValue = rootNode.getLeftChild().getValue();
-//        int rightChildValue = rootNode.getRightChild().getValue();
-//        if (element < rootNodeValue) {
-////            if (rootNode.leftChild == null) {
-//            if (rootNode.getLeftChild() == null) {
-////                rootNode.leftChild = new Node(element);
-////                rootNode.setLeftChild(  ?   );
-//                Node leftChild = new Node(element);
-//                rootNode.setLeftChild(leftChild);
-//            } else {
-////                leftChild.addElement(element);
-//               //rootNode.getLeftChild().addElement(element);
-//                Node leftChild = new Node(element);
-//                rootNode = rootNode.getLeftChild();
-//                rootNode.setLeftChild(leftChild);
-//            }
-//        } else {
-////            if (rootNode.rightChild == null) {
-//            if (rootNode.getRightChild() == null) {
-////                rightChild = new Node(element);
-//                Node rightChild = new Node(element);
-//                rootNode.setRightChild(rightChild);
-//            } else {
-////                rightChild.addElement(element);
-//                Node rightChild = new Node(element);
-//                rootNode = rootNode.getRightChild();
-//                rootNode.setRightChild(rightChild);
-//            }
-//        }
-//    }
-
-////    // TODO: rename "latest" to something more fitting
-//    public Node addElement(Node latest, int element) {
-//        if (latest == null) {
-//            return new Node(element);
-//        }
-//        if (element < latest.getValue()) {
-//            latest.setLeftChild(addElement(latest.getLeftChild(), element));
-//        } else if (element > latest.getValue()) {
-//            latest.setRightChild(addElement(latest.getRightChild(), element));
-//        } else {
-//            return latest;
-//        }
-//        return latest;
-//    }
-
-//    //for first node
-//    public void addElement(int element) {
-//        if (numElements == 0) {
-//            createRootNode(element);
-//        }
-
     public void addElement(int value) {
         addElement(this.rootNode, value);
     }
-    
+
     public void addElement(Node currentNode, int element) {
         if (numElements == 0) {
             createRootNode(element);
@@ -128,30 +71,6 @@ public class BinaryTreeImpl implements BinaryTree {
         }
         return false;
     }
-
-//    //TODO: finish
-//    @Override
-//    public int getLeftChild(int element) throws ChildNotFoundException {
-//        // Node where value = element
-//        // get leftChild property
-//        Node latest = rootNode;
-//        while (latest != null) {
-//            if (latest.getValue() == element) {
-//                return latest.getLeftChild().getValue();
-//            } else if (latest.getValue() > element) {
-//                latest = latest.getRightChild();
-//                getLeftChild(element);
-//            } else if (latest.getValue() < element) {
-//                latest = latest.getLeftChild();
-//                getLeftChild(element);
-//            }
-//        }
-//    }
-
-//    @Override
-//    public int getRightChild(int element) throws ChildNotFoundException {
-//        return 0;
-//    }
 
     @Override
     public int[] getSortedTreeAsc() {

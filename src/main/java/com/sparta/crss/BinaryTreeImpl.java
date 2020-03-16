3eea4affde8f02622201f7370f73f90252ced3ff
Colette Shaw <CShaw@spartaglobal.com>
2020-03-16 10:58:57 +0000

    Refactor messy method

--- src/main/java/com/sparta/crss/BinaryTreeImpl.java
+++ src/main/java/com/sparta/crss/BinaryTreeImpl.java
@@ -1,11 +1,14 @@
 package com.sparta.crss;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class BinaryTreeImpl implements BinaryTree {
 
     private Node rootNode;
     private int numElements;
+    int[] resultArray = new int[numElements];
+    ArrayList<Integer> resultArrayList = new ArrayList<Integer>();
 
     public BinaryTreeImpl(int element) {
         createRootNode(element);
@@ -73,60 +76,90 @@ public class BinaryTreeImpl implements BinaryTree {
         return false;
     }
 
+    //private int[] resultArray = new int[numElements];
+
+    public int[] print(Node current) {
+        int i = 0;
+        if (current == null) {
+            return resultArray;
+        } else {
+            print(current.getLeftChild());
+            print(current.getRightChild());
+            resultArray[i] = current.getValue();
+            i++;
+            return resultArray;
+        }
+    }
+
     @Override
     public int[] getSortedTreeAsc() {
-        int[] resultArray = new int[numElements];
-        int counter = 0;
-        int lastValue;
-        Node currentNode = rootNode;
-        while (counter <= numElements) {
-            lastValue = currentNode.getValue();
-            resultArray[counter] = lastValue;
-            counter++;
-            if (currentNode.getLeftChild() != null) {
-                currentNode = currentNode.getLeftChild();
-                lastValue = currentNode.getValue();
-                resultArray[counter] = lastValue;
-                counter++;
-            } else if (currentNode.getRightChild() != null) {
-                currentNode = currentNode.getRightChild();
-                lastValue = currentNode.getValue();
-                resultArray[counter] = lastValue;
-                counter++;
-            } else {
-                currentNode = currentNode.getParent();
-            }
-        }
-        Arrays.sort(resultArray);
+//        int[] resultArray = new int[numElements];
+
+//        int counter = 0;
+//        int lastValue;
+//        Node currentNode = rootNode;
+//        while (counter <= numElements) {
+//            lastValue = currentNode.getValue();
+//            resultArray[counter] = lastValue;
+//            counter++;
+//            if (currentNode.getLeftChild() != null) {
+//                currentNode = currentNode.getLeftChild();
+//                lastValue = currentNode.getValue();
+//                resultArray[counter] = lastValue;
+//                counter++;
+//            } else if (currentNode.getRightChild() != null) {
+//                currentNode = currentNode.getRightChild();
+//                lastValue = currentNode.getValue();
+//                resultArray[counter] = lastValue;
+//                counter++;
+//            } else {
+//                currentNode = currentNode.getParent();
+//            }
+//        }
+//        Arrays.sort(resultArray);
+
         return resultArray;
     }
 
     @Override
     public int[] getSortedTreeDesc() {
+        return getSortedTreeDesc(rootNode);
+    }
+
+    public int[] getSortedTreeDesc(Node current) {
 //        return BinaryTreeImpl.getSortedTreeAsc().reverse();
-        int[] resultArray = new int[numElements];
-        int counter = 0;
-        int lastValue;
-        Node currentNode = rootNode;
-        while (counter <= numElements) {
-            lastValue = currentNode.getValue();
-            resultArray[counter] = lastValue;
-            counter++;
-            if (currentNode.getLeftChild() != null) {
-                currentNode = currentNode.getLeftChild();
-                counter++;
-            } else if (currentNode.getRightChild() != null) {
-                currentNode = currentNode.getRightChild();
-                counter++;
-            } else {
-                currentNode = currentNode.getParent();
-            }
-        }
-        Arrays.sort(resultArray);
-        int[] resultArrayReversed = new int[resultArray.length];
-        for (int i = 0; i < resultArray.length; i++) {
-            resultArrayReversed[0] = resultArray[resultArray.length - i];
-        }
-        return resultArrayReversed;
+
+//        int[] resultArray = new int[numElements];
+//        int counter = 0;
+//        int lastValue;
+//        Node currentNode = rootNode;
+//        while (counter <= numElements) {
+//            lastValue = currentNode.getValue();
+//            resultArray[counter] = lastValue;
+//            counter++;
+//            if (currentNode.getLeftChild() != null) {
+//                currentNode = currentNode.getLeftChild();
+//                counter++;
+//            } else if (currentNode.getRightChild() != null) {
+//                currentNode = currentNode.getRightChild();
+//                counter++;
+//            } else {
+//                currentNode = currentNode.getParent();
+//            }
+//        }
+//        Arrays.sort(resultArray);
+//        int[] resultArrayReversed = new int[resultArray.length];
+//        for (int i = 0; i < resultArray.length; i++) {
+//            resultArrayReversed[0] = resultArray[resultArray.length - i];
+//        }
+//        return resultArrayReversed;
+
+//        if (current != null) {
+//            resultArrayList.add(current.getValue());
+//            getSortedTreeDesc(current.getLeftChild());
+//        }
+//        current = current.getParent();
+
+        return new int[0];
     }
 }
